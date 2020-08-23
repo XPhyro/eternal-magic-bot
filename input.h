217d6409c3d0b344e941cb72bdc2a10f09a0f8fb
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-08-23 21:50:13 +0300

    Fix header preprocessors

--- input.h
+++ input.h
@@ -1,8 +1,8 @@
 #ifndef EM_RED_TO_BLUE_INPUT_H
 #define EM_RED_TO_BLUE_INPUT_H
 
-#endif //EM_RED_TO_BLUE_INPUT_H
-
 int getsize(const char *msg, const char *errmsg);
 
 char *getmat(int w, int h, const char *msg, const char *errmsg);
+
+#endif //EM_RED_TO_BLUE_INPUT_H
